51c00ac108ffaf58a8907be54232f4dede5450f7
Daniel Ferreira <danielse@ifce.edu.br>
2023-11-01 08:54:13 -0300

    Aula 01/11

--- Estruturas/s3.c
+++ Estruturas/s3.c
@@ -9,20 +9,26 @@ struct Produto{
 
 #define QTD_PRODUTOS 10
 
-void criarEstoque(struct Produto *p, int q);
-void exibirEstoque(struct Produto *p, int q);
+void criarEstoque(struct Produto *, int);
+void exibirEstoque(struct Produto *, int);
+// struct Produto *getMaiorPrecoEstoque(struct Produto *, int);
+void getMaiorPrecoEstoque(struct Produto *, int, struct Produto **);
+void exibirProduto(char *, struct Produto *);
+
 
 int main(){
     struct Produto estoque[QTD_PRODUTOS];
 
+    srand(time(NULL));
     criarEstoque(estoque, QTD_PRODUTOS);
 
     exibirEstoque(estoque, QTD_PRODUTOS);
 
-    // struct Produto *prodMaiorPreco;
-    // getMaiorPrecoEstoque(estoque, QTD_PRODUTOS, prodMaiorPreco);
+    struct Produto *prodMaiorPreco;
+    getMaiorPrecoEstoque(estoque, QTD_PRODUTOS, &prodMaiorPreco);
 
-    // exibirProduto(prodMaiorPreco);
+    exibirProduto("Produto de Maior Valor : ", prodMaiorPreco);
+    
 
     return 0;
 
@@ -43,4 +49,34 @@ void exibirEstoque(struct Produto *p, int q){
         printf("Cod: %d\n", (p+k)->cod);
         printf("Valor: %.2f\n", (p+k)->preco); 
     }
+}
+
+void getMaiorPrecoEstoque(struct Produto *p, int q, struct Produto **pMaiorPreco){
+
+    *pMaiorPreco = p;
+    for (int k=1; k<q; k++){
+        if ( (p + k)->preco > ((*pMaiorPreco)->preco) ){
+            *pMaiorPreco = p+k;
+        }
+    }
+    
+}
+
+// struct Produto *getMaiorPrecoEstoque(struct Produto *p, int q){
+
+//     struct Produto *pMaiorPreco = p;
+//     for (int k=1; k<q; k++){
+//         if ( (p + k)->preco > (pMaiorPreco->preco) ){
+//             pMaiorPreco = p+k;
+//         }
+//     }
+
+//     return pMaiorPreco;
+// }
+
+void exibirProduto(char *msg, struct Produto *pprod){
+    printf("#### \n\t\t %s \n#####\n",msg);
+    printf("Cod: %d\n", pprod->cod);
+    printf("Valor: %.2f\n", pprod->preco);
+    printf("\n");
 }
